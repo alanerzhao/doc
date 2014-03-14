8327b648d9c33d0289e1075d61554b96bdd05ba7
baozi <alanerzhao@gmail.com>
2014-03-14 11:37:59 +0800

    增加vim替换空格

--- document/vim_doc.md
+++ document/vim_doc.md
@@ -133,6 +133,19 @@ vimwiki
 map <S-F4> :VimwikiAll2HTML<cr>
 map <F4> :Vimwiki2HTML<cr>
 
+在vim中，有时需要将tab转换成space。使用ret命令（replace tab）。
+[range]ret[ab]! [new-tabstop]
+
+举例：将第一行到文件尾的tab转换成space，每个tab用4个space替代。
+:set expandtab
+:%ret! 4
+如果没有给定4，则用当前的tab宽度设定替换为space。
+
+其它相关命令：
+:set tabstop=4        设定tab宽度为4个字符
+:set shiftwidth=4     设定自动缩进为4个字符
+:set expandtab        用space替代tab的输入
+:set noexpandtab      不用space替代tab的输入
 ----- end 80 page ------
 
 
--- mvc/Controller.js
+++ mvc/Controller.js
@@ -4,25 +4,34 @@ var exports = this;
 	var mod = {};
 	mod.create = function(includes) {
 		var result = function() {
-            //执行第个实例的init构造属性
+			//执行第个实例的init构造属性
 			this.init.apply(this, arguments);
 		};
+
 		result.fn = result.prototype;
+        
 		result.fn.init = function() {}
+
 		result.proxy = function(func) {
 			return $.proxy(func, this);
 		}
+
 		result.fn.proxy = result.proxy;
+        //给实例继承属性或方法
 		result.include = function(obj) {
 			$.extend(this.fn, obj);
 		}
+
 		result.extend = function(obj) {
 			$.extend(this, obj);
 		}
 		if (includes) result.include(includes);
+
+        //返回对象
 		return result;
 
 	}
+    //暴露接口
 	exports.Controller = mod;
 
 })(jQuery);
@@ -31,17 +40,22 @@ var exports = this;
 
 jQuery(function($) {
 	exports.SearchView = Controller.create({
-        //把对应的key通过$传换成jquery对象存起来
+        //把jquery对象映射成js对象
 		elements: {
 			"input[type=search]": "searchInput",
 			"form": "searchForm"
 		},
+        
+        //要绑定的事件
+		events: {
+			"submit form": "search"
+		},
 
 		init: function(element) {
-            console.log(element)
-			this.el = $(element);
-			this.refreshElements();
-			this.searchForm.submit(this.proxy(this.search));
+
+            this.el = $(element); 
+            this.refreshElements();
+			this.delegateEvents();
 		},
 
 		search: function() {
@@ -50,19 +64,42 @@ jQuery(function($) {
 		},
 
 		// Private
+        //映射helper
 		$: function(selector) {
 			return $(selector, this.el);
 		},
 
-        //映射elements表
+        //映射
 		refreshElements: function() {
 			for (var key in this.elements) {
 				this[this.elements[key]] = this.$(key);
 			}
+		},
+
+		eventSplitter: /^(\w+)\s*(.*)$/,
+
+		delegateEvents: function() {
+			for (var key in this.events) {
+				var methodName = this.events[key];
+				var method = this.proxy(this[methodName]);
+
+				var match = key.match(this.eventSplitter);
+				var eventName = match[1],
+				selector = match[2];
+
+				if (selector === '') {
+					this.el.bind(eventName, method);
+				} else {
+                    //事件委托
+					this.el.delegate(selector, eventName, method);
+				}
+			}
 		}
 	});
 
-    
+
+
+    /* 写代码的思想好牛b */
 	console.dir(new SearchView("#users"));
 });
 
